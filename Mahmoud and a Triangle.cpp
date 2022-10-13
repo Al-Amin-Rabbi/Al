8a82952fa1ecc17a4f659a37f9cfdc849ebe823b
Al-Amin-Rabbi <107281719+Al-Amin-Rabbi@users.noreply.github.com>
2022-10-13 20:19:42 +0600

    Add files via upload

---
+++ Mahmoud and a Triangle.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    int arr[x];
+    int tc=0;
+    for(int i=0; i<x; i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+x);
+    for(int i=0; i<x-2; i++)
+    {
+        if(arr[i]+arr[i+1]>arr[i+2] &&
+                arr[i]+arr[i+2]>arr[i+1] &&
+                arr[i+2]+arr[i+1]>arr[i]
+          )
+        {
+            cout<<"YES"<<endl;
+            tc=1;
+            break;
+        }
+
+    }
+    if(tc==0)
+    cout<<"NO"<<endl;
+    return 0;
+}
